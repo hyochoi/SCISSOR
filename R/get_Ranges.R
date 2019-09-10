a9d078f3f2cf427da39826383c3f0bcc8621f0d0
Hyo Young Choi <lucienne1216@gmail.com>
2019-09-10 18:15:21 -0500

    get_Ranges updated

--- R/get_Ranges.R
+++ R/get_Ranges.R
@@ -1,25 +1,35 @@
 #' Genomic ranges and locus ranges
 #'
-#' Get genomic ranges and locus ranges from the regions provided
+#' This function returns
 #'
-#' @param regions genomic regions formatted as chr1:1-100,200-300:+"
-#' @param outputType type of intronic region that will be included in output,
-#'   with choices "whole_intron", "part_intron", or "only_exon"; the second is
-#'   the default.
+#' @param Gene character(1) identifying the gene symbol, e.g. \code{"TP53"}
+#' @param regions character(1) describing genomic regions formatted as
+#'   \code{"chr1:1-100,200-300:+"}
+#' @param outputType character(1) of the type of intronic region that will be
+#'   included in output, with choices "whole_intron", "part_intron", or
+#'   "only_exon"; the second is the default.
+#'
+#' @return a \code{list} of key information for the regions provided:
+#' \itemize{
+#'   \item Gene gene symbol
+#'   \item gRanges ranges
+#' }
+#'
 #' @examples
 #' regions="chr17:7571720-7573008,7573927-7574033,7576525-7576657,7576853-7576926,7577019-7577155,7577499-7577608,7578177-7578289,7578371-7578554,7579312-7579590,7579700-7579721,7579839-7579940:-"
-#' Ranges=get_Ranges(regions=regions)
+#' Ranges=get_Ranges(Gene="TP53",regions=regions,outputType="part_intron")
 #'
 #' @import BiocManager Rsamtools
 #' @export
 get_Ranges = function(Gene=NULL,regions,outputType="part_intron") {
-  require(Rsamtools)
 
+  require(Rsamtools)
   if (missing(regions)) {
     stop("regions must be specified")
   }
-  if (is.null(Gene)) {
-    warning("The gene name is missing")
+  if (length(Gene)>1) {
+    warning("More than one gene was provided. The first gene will be used.")
+    Gene=Gene[1]
   }
   chr = strsplit(regions,":")[[1]][1]
   strtend = do.call(rbind,strsplit(strsplit(strsplit(regions,":")[[1]][2],",")[[1]],"-"))
@@ -35,28 +45,28 @@ get_Ranges = function(Gene=NULL,regions,outputType="part_intron") {
     intron.len = ceiling(len.intron.hy(exon=regions)*0.5);
     ep.new = find.exon.hy(regions,is.intron=TRUE,num.intron=0) ;
   }
-  intron.len = ceiling(len.intron.hy(exon=regions)*0.5);
-  ep.new = find.exon.hy(regions,is.intron=TRUE,num.intron=intron.len) ;
   lRanges0=ep.new$ep
   gRanges0=cbind(strtend.num[,1]-(lRanges0[,2]-lRanges0[,1]),strtend.num,strtend.num[,2]+(lRanges0[,4]-lRanges0[,3]))
+  cRanges0=find.exon.hy(regions,is.intron=TRUE,num.intron=0)$ep[,c(2,3)]
   new.regions=paste0(chr,":",paste(apply(gRanges0[,c(1,4)],1,function(x) paste(x,collapse="-")),collapse=","),":",strnd)
 
   if (strnd=="+") {
     gRanges=gRanges0
     lRanges=lRanges0
-    colnames(gRanges)=c("ip.start","e.start","e.end","ip.end") # e:exon; ip: intronic part;
-    rownames(gRanges)=paste("exon",1:nrow(gRanges),sep="")
-    colnames(lRanges)=c("ip.start","e.start","e.end","ip.end")
-    rownames(lRanges)=paste("exon",1:nrow(gRanges),sep="")
+    cRanges=cRanges0
   } else {
     gRanges=gRanges0[rev(1:nrow(gRanges0)),rev(1:4)]
     lRanges=max(lRanges0)-lRanges0[rev(1:nrow(lRanges0)),rev(1:4)]+1
-    colnames(gRanges)=c("ip.start","e.start","e.end","ip.end") # e:exon; ip: intronic part;
-    rownames(gRanges)=paste("exon",1:nrow(gRanges),sep="")
-    colnames(lRanges)=c("ip.start","e.start","e.end","ip.end")
-    rownames(lRanges)=paste("exon",1:nrow(gRanges),sep="")
+    cRanges=max(cRanges0)-cRanges0[rev(1:nrow(cRanges0)),2:1]+1
   }
-  rm(gRanges0,lRanges0)
-  output=list(Gene=Gene,gRanges=gRanges,lRanges=lRanges,chr=chr,strand=strnd,regions=regions,new.regions=new.regions)
+  rm(gRanges0,lRanges0,cRanges0)
+  colnames(gRanges)=c("ip.start","e.start","e.end","ip.end") # e:exon; ip: intronic part;
+  rownames(gRanges)=paste("exon",1:nrow(gRanges),sep="")
+  colnames(lRanges)=c("ip.start","e.start","e.end","ip.end")
+  rownames(lRanges)=paste("exon",1:nrow(gRanges),sep="")
+  colnames(cRanges)=c("e.start","e.end")
+  rownames(cRanges)=paste("exon",1:nrow(gRanges),sep="")
+
+  output=list(Gene=Gene,gRanges=gRanges,lRanges=lRanges,cRanges=cRanges,chr=chr,strand=strnd,regions=regions,new.regions=new.regions)
   output
 }
