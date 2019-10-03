65ca010914bd1e8050221650d760334dff8e83e1
Hyo Young Choi <lucienne1216@gmail.com>
2019-10-03 18:50:45 -0500

    plotting

--- R/plot_ScissorOS.R
+++ R/plot_ScissorOS.R
@@ -1,37 +1,71 @@
 #' Plot outlyingness scores with a kernel density estimate
+#'
+#' @param object For the classes of GSCOutput and LSCOutput, the arguments GSC
+#'   and LSC will be ignored.
+#' @param GSC logical, whether to plot the OS from GSC. Default is TRUE
+#' @param LSC logical, whether to plot the OS from LSC. Default is TRUE
+#' @param colmat
+#' @param textSC
+#'
 #' @export
 plot_ScissorOS = function(object,GSC=TRUE,LSC=TRUE,
                           colmat=NULL,textSC=TRUE) {
+  if (missing(object)) {
+    stop("Object is missing. An object for plotting must be specified")
+  }
+  stopifnot(is(object,c("ScissorOutput","GSCOutput","LSCOutput")))
 
-  n = ncol(object$datalog)
-  outliers1=object$outliers1
-  outliers2=object$outliers2
-  if (is.null(colmat)) {
-    palette.hy()
-    colmat = rep("darkgrey",n);
-    if (length(outliers1)>0) {
-      colmat[outliers1] = 1;
+  if (is(object,"ScissorOutput")) {
+    n = ncol(object$logData)
+    globalSC = object$globalSC
+    localSC = object$localSC
+    globalOS = object$GSCout$OS
+    localOS = object$LSCout$OS
+    globalCutoff = object$GSCout$cutoff
+    localCutoff = object$LSCout$cutoff
+  } else {
+    if (is(object,"GSCOutput")) {
+      n = length(object$OS)
+      globalSC = object$SC
+      localSC = c()
+      globalOS = object$OS
+      globalCutoff = object$GSCout$cutoff
+      GSC = TRUE
+      LSC = FALSE
+    } else {
+      n = length(object$OS)
+      globalSC = object$ignoredCases
+      localSC = object$SC
+      localOS = object$OS
+      localCutoff = object$LSCout$cutoff
+      GSC = FALSE
+      LSC = TRUE
     }
-    if (length(outliers2)>0) {
-      colmat[outliers2] = 2;
+  }
+  if (is.null(colmat)) {
+    palette_SCISSOR()
+    colmat = rep("darkgrey",n);
+    if (length(globalSC)>0) {
+      colmat[globalSC] = 1;
+    }
+    if (length(localSC)>0) {
+      colmat[localSC] = 2;
     }
   }
   if (GSC) {
-    kdeplot.hy(object$GSCout$OS,indlist=outliers1,main="Outlyingness Scores from Global",
+    kdeplot.hy(globalOS,indlist=globalSC,main="Outlyingness Scores from Global",
                text=textSC,high=0.95,low=0.1,colmat=colmat)
-    abline(v=object$GSCout$cutoff)
+    abline(v=globalCutoff)
     legend("topright",bty="n",
-           legend=c(paste("# GSCs detected =",length(outliers1))))
+           legend=c(paste("# GSCs detected =",length(globalSC))))
   }
   if (LSC) {
-    kdeplot.hy(object$LSCout$OS,indlist=outliers2,
+    kdeplot.hy(localOS,indlist=localSC,
                main="Outlyingness Scores from Local",
                text=textSC,colmat=colmat,
                high=0.95,low=0.1)
-    abline(v=object$LSCout$cutoff)
+    abline(v=localCutoff)
     legend("topright",bty="n",
-           legend=c(paste("# LSCs detected =",length(outliers2)),
-                    paste("K-S df     =",object$LSCout$ks.df),
-                    paste("K-S pval   =",round(object$LSCout$ks.pval,5))))
+           legend=c(paste("# LSCs detected =",length(localSC))))
   }
 }
