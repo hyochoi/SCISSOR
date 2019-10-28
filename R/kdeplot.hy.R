7465512b9b6746a5f60509ad13e9ef999ec6bbd1
Hyo Young Choi <lucienne1216@gmail.com>
2019-10-28 17:02:48 -0500

    bug fixed

--- R/kdeplot.hy.R
+++ R/kdeplot.hy.R
@@ -3,7 +3,7 @@
 kdeplot.hy <- function(x, bandwidth=NULL, indlist=NULL, indlist2=NULL,
                        colmat=NULL,indcol1="red", indcol2="cadetblue",
                        text=FALSE,textwhat=NULL,
-                       indpch=16, indcex=0.9, low=0.4, high=0.7,
+                       pch=16, indcex=1.2, low=0.4, high=0.7,
                        line.col="black", line.lwd=1,
                        xlab=NULL, ylab=NULL, xlim=NULL, ylim=NULL,main=NULL,cex.main=1.2,
                        ADlegend=FALSE,...){
@@ -23,54 +23,52 @@ kdeplot.hy <- function(x, bandwidth=NULL, indlist=NULL, indlist2=NULL,
   ymax <- max(x.kde$y)*high
   yplot <- ymin + (ymax - ymin)*runif(n)
   if (is.null(xlim)){
-    xlim=c(min(x), max(x)) ;
+    xlim=c(min(x), max(x))
   }
-  plot(x.kde, type='l', xlim=xlim, ylim=ylim, axes=F, ylab=NA, xlab=NA, col=line.col, lwd=line.lwd) ;
+  plot(x.kde, type='l', xlim=xlim, ylim=ylim, axes=F, ylab=NA, xlab=NA, col=line.col, lwd=line.lwd)
   box() ;
   abline(0, 0, lty=2, col=rgb(.5, .5, .5))
   abline(v=0, lty=2, col=rgb(.5, .5, .5))
-  axis(side=1, tck=-.015,labels=NA) ;
-  axis(side=1, lwd=0, line=-1, cex=0.2,cex.axis=0.9) ;
+  axis(side=1, tck=-.015,labels=NA)
+  axis(side=1, lwd=0, line=-1, cex=0.2,cex.axis=0.9)
   axis(side=2, tck=-0.015,lwd=0,line=-1,cex.axis=0.9)
   # axis(side=2, tck=-.015,labels=NA) ;
   # axis(side=2, lwd=0, line=-1, cex=0.2) ;
-  mtext(side=1, xlab, line=1.5, cex=1.3) ;
-  mtext(side=2, ylab, line=1.5, cex=1.3) ;
+  mtext(side=1, xlab, line=1.5, cex=1.3)
+  mtext(side=2, ylab, line=1.5, cex=1.3)
 
   title(main, cex.main=cex.main, font.main=1, line=0.3) ;
 
   if (length(indlist)==0) {indlist=NULL}
   if (is.null(indlist)){
     if (is.null(colmat)){
-      colmat <- rep("grey", length(x)) ;
+      colmat <- rep("grey", length(x))
     }
-    points(x, yplot, col=colmat, ...) ;
-  }
-  if (!is.null(indlist)){
+    points(x, yplot, col=colmat, pch=pch, ...)
+  } else {
     if (is.null(colmat)){
-      colmat <- rep("grey", length(x)) ;
+      colmat <- rep("grey", length(x))
       if (!is.null(indlist2)){
         colmat[indlist2] = rep(indcol2,length(indlist2))
       }
-      colmat[indlist] <- rep(indcol1, length(indlist)) ;
+      colmat[indlist] <- rep(indcol1, length(indlist))
     }
-    indlist0 = unique(indlist,indlist2);
-    points(x[-indlist0], yplot[-indlist0], col=colmat[-indlist0], ...) ;
+    indlist0 = unique(indlist,indlist2)
+    points(x[-indlist0], yplot[-indlist0], col=colmat[-indlist0], pch=pch, ...)
     if (!text){
+      points(x[indlist], yplot[indlist], col=colmat[indlist], cex=indcex, pch=pch)
       if (!is.null(indlist2)){
-        points(x[indlist2], yplot[indlist2], col=colmat[indlist2], cex=indcex, pch=indpch) ;
+        points(x[indlist2], yplot[indlist2], col=colmat[indlist2], cex=indcex, pch=pch)
       }
-      points(x[indlist], yplot[indlist], col=colmat[indlist], cex=indcex, pch=indpch) ;
-    }
-    if (text){
+    } else {
       if (!is.null(indlist2)){
-        text(x[indlist2], yplot[indlist2], indlist2, col=colmat[indlist2], cex=indcex) ;
+        text(x[indlist2], yplot[indlist2], indlist2, col=colmat[indlist2], cex=indcex)
       }
       if (is.null(textwhat)) {
-        text(x[indlist], yplot[indlist], indlist, col=colmat[indlist], cex=indcex) ;
+        text(x[indlist], yplot[indlist], indlist, col=colmat[indlist], cex=indcex)
       } else {
         yplot[indlist] = seq(min(yplot[indlist]),max(yplot[indlist]),length.out=length(indlist))
-        text(x[indlist], yplot[indlist], textwhat, col=colmat[indlist], cex=indcex) ;
+        text(x[indlist], yplot[indlist], textwhat, col=colmat[indlist], cex=indcex)
       }
     }
   }
