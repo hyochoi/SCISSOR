585411a8339646f80ad044be3d13ddf52c415648
Hyo Young Choi <lucienne1216@gmail.com>
2020-02-15 14:49:30 -0600

    get_PO dimension error fixed

--- R/get_PO.R
+++ R/get_PO.R
@@ -1,6 +1,6 @@
 #'
 #' @export
-get_PO = function(X,siglev=1e-4,NormCutoff=3,canDir=NULL,numSearch=300) {
+get_PO = function(X,siglev=1e-4,NormCutoff=3,canDir=NULL,numSearch=300,qrsc=FALSE) {
 
   if (is.null(X)) {
     stop("object is null")
@@ -25,20 +25,24 @@ get_PO = function(X,siglev=1e-4,NormCutoff=3,canDir=NULL,numSearch=300) {
   } else { # X is a matrix
     if (is.null(canDir)) {
       n = ncol(X); M = nrow(X);
+      z.pca = pca.hy(X)
       Scov = (X%*%t(X))/n
-      d = length(which(eigen(Scov)$values>1e-13))
+      p = length(which(z.pca$eigenval>1e-13))
+      if (p < M) {
+        X = z.pca$projmat[1:p,]
+      }
       x = t(X)
       ndir = numSearch*M
       A = generdir(x,ndir=ndir) # generates `ndir' directions (ndir by M)
 
       ## Add more potential directions
-      if (M == d) {
+      if (M == p) {
         B0 = solve(Scov)%*%X   # M by n
         B0 = B0[,-which(apply(B0,2,FUN=function(x){sqrt(sum(x^2))})<1e-10)]
         B1 = sweep(B0,2,apply(B0,2,FUN=function(x){sqrt(sum(x^2))}),"/")  # normalized M by n
         A = rbind(A,t(B1))  # ndir by M
       }
-      A = rbind(A,diag(M))
+      A = rbind(A,diag(p))
       ## Compute projection outlyingness
       Y = x %*% t(A) # project x onto A (n by ndir)
       ADstat = apply(Y,2,ADstatWins.hy) #
@@ -46,10 +50,13 @@ get_PO = function(X,siglev=1e-4,NormCutoff=3,canDir=NULL,numSearch=300) {
       if (length(indir)>1) {
         A = A[indir,]; Y = Y[,indir]
         out_temp = apply(X=Y,MARGIN=2,
-                         FUN= function(t) (t-median(t))/mad(t)) # n by length(indir)
+                         FUN= function(t) pd.rate.hy(t,qrsc=qrsc)) # n by length(indir)
         indexmax = apply(abs(out_temp),1,which.max)
         NPS = out_temp[,indexmax]
         directions=t(A[indexmax,])
+        if (p < M) {
+          directions = z.pca$dirmat[,1:p]%*%directions
+        }
 
         neg.index = which(diag(NPS)<0)
         if (length(neg.index)>0) {
@@ -57,8 +64,8 @@ get_PO = function(X,siglev=1e-4,NormCutoff=3,canDir=NULL,numSearch=300) {
           directions[,neg.index] = -directions[,neg.index];
         }
         OS=diag(NPS)
-        OSpval = 1-pchisq(q=OS^2,df=d);
-        cutoff = qchisq(p=(1-siglev),df=d);
+        OSpval = 1-pchisq(q=OS^2,df=p);
+        cutoff = qchisq(p=(1-siglev),df=p);
       } else {
         message("no direction satisfied the constraint")
         OS = rep(0,n)
