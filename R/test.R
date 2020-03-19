194ef36e34061718fa433bfab404e1396cc5b2c0
lucienne1216@gmail.com <lucienne1216@gmail.com>
2020-03-19 11:31:34 -0500

    test functions updated

--- R/test.R
+++ R/test.R
@@ -1,3 +1,347 @@
+#' Detect local shape changes
+#'
+#' @export
+miscGlobal_test = function(inputData,Ranges,JSR.table,
+                           siglev=1e-04,ADcutoff=3) {
+
+  plat.junctions = plat_gene(Ranges=Ranges,JSR.table=JSR.table)
+  plat.table = build_platTable(Ranges=Ranges,JSR.table=JSR.table)
+  plat.baseMat = build_baseMat(plat.table)
+
+  ## 1. Detecting outliers from known directions
+  knownDir = build_knownDir(Ranges=Ranges,JSR.table=JSR.table)
+  platBasisDir = apply(plat.baseMat,2,FUN=function(t){t/sqrt(sum(t^2))})
+  KnownBasisDir = apply(plat.baseMat%*%knownDir,2,FUN=function(t){t/sqrt(sum(t^2))})
+
+  normProjData = t(platBasisDir) %*% normData # low-dimensional data object to be used for outlier detection
+  adj.knownDir = sqrt(t(plat.baseMat)%*%plat.baseMat)%*%knownDir # adjust size of plats
+
+  # Get primary projection outlyingness
+  knownPOmat = get_POgivenB(X=normProjData,B=adj.knownDir)
+  knownPO = diag(knownPOmat[apply(abs(knownPOmat),2,which.max),])
+  # knownPODir = knownDir[,apply(abs(knownPOmat),2,which.max)]
+
+  # Get sum of squares of the residuals
+  knownROmat = get_resdZsum(X=normProjData,B=adj.knownDir,L1=FALSE) # Residual (Z-score) outlyingness
+  knownRO = diag(knownROmat[apply(knownROmat,2,which.min),])
+  knownRODir = knownDir[,apply(knownROmat,2,which.min)]
+  knownPOminRO = diag(knownPOmat[apply(knownROmat,2,which.min),])
+
+  ks.pval=ks.stat=rep(0,20)
+  temp.zero=which(knownPO^2 < 1e-10)
+  for (df in 1:20) {
+    # hist(pchisq(onoff_stat^2,df=df,lower.tail=F),main=df)
+    temp.out=which(knownPO>sqrt(qchisq(0.001,df=df,lower.tail=F)))
+    ks.output=ks.test(knownPO[which(! c(1:length(knownPO)) %in% c(temp.out,temp.zero))]^2,pchisq,df)
+    ks.pval[df]=ks.output$p.value
+    ks.stat[df]=ks.output$statistic
+    # cat(paste(df,"|",round(ks.pval[df],digits=5),"|",
+    #           round(ks.stat[df],digits=3)),"\n")
+  }
+  df=which.min(ks.stat)
+  cutoff = sqrt(qchisq(siglev,df=df,lower.tail=F))
+  # cutoff = sqrt(qchisq(p=(1-siglev),df=11))
+
+  # Save results
+  OStmp = abs(knownPOminRO)
+  MODtmp = knownRODir
+  OUTtmp = which(OStmp>cutoff)
+
+  ## 2. Detecting outliers from ATS/ATT directions
+  atDir = build_atDir(inputData=normData,Ranges=Ranges,JSR.table=JSR.table,
+                      cutoff=cutoff)
+  adj.atDir = sqrt(t(plat.baseMat)%*%plat.baseMat)%*%atDir
+  atPOmat = get_POgivenB(X=normProjData,B=adj.atDir)
+  # atPO = diag(atPOmat[apply(abs(atPOmat),2,which.max),])
+  atPODir = atDir[,apply(abs(atPOmat),2,which.max)]
+  atROmat = get_resdZsum(X=normProjData,B=adj.atDir,L1=FALSE)
+  atRO = diag(atROmat[apply(atROmat,2,which.min),])
+  atPOminRO = diag(atPOmat[apply(atROmat,2,which.min),])
+  atSC = which((abs(atPOminRO)>cutoff) & ((abs(atPOminRO)>abs(knownPOminRO)) & (atRO<knownRO)))
+
+  ## Update the OS and MOD
+  OStmp[atSC] = atPOminRO[atSC]
+  MODtmp[,atSC] = atPODir[,atSC]
+  colnames(MODtmp)[atSC] = colnames(atPODir)[atSC]
+
+  ## Get final results table
+  outliers = which(OStmp>cutoff)
+  globalResult.table = data.frame(Outlier=outliers,
+                                  Statistic=round(OStmp[outliers],digits=3),
+                                  Region=locate_region(tag=colnames(MODtmp[,outliers]),Ranges=Ranges,JSR.table=JSR.table),
+                                  Tag=colnames(MODtmp[,outliers]))
+
+  ## Output
+  outputObject = list(table=globalResult.table,
+                      Outlier=globalResult.table$Outlier,
+                      OS=OStmp,
+                      platBasis=plat.baseMat,
+                      platMOD=MODtmp,
+                      cutoff=cutoff)
+  class(outputObject) = append(class(outputObject),"GSCOutput")
+  return(outputObject)
+}
+
+#' Detect local shape changes
+#'
+#' This function discovers outlying subjects whose RNA-seq have "local" abnormal shapes
+#' and provides the most outlying window-direction for each outlier.
+#'
+#' @param miscGlobalResult Result from \link{miscGlobal}
+#' @param inputData normalized coverage matrix from \link{process_data}
+#' @param pileupData raw coverage matrix, dataI from \link{process_data}
+#' @param Ranges
+#' @param JSR.table
+#' @param windowSize a window length. Default is 100.
+#' @param siglev the significance level for a robust outlier detection. Default is 1e-5.
+#' IF cutoff is specified, siglev is not used.
+#' @param cutoff the cutoff value for outlying statistics.
+#' If NULL, the cutoff value is computed based on the specified siglev.
+#'
+#' @import zoo
+#' @export
+miscLocal_test = function(miscGlobalResult,
+                          inputData,pileupData,
+                          Ranges,JSR.table,
+                          siglev=1e-4,cutoff=NULL,
+                          ADcutoff=3,windowSize=50) {
+  n = ncol(inputData); d = nrow(inputData);
+  if (is.null(cutoff)) {
+    cutoff = miscGlobalResult$cutoff
+  }
+  ## 1. Detect outliers from cryptic junction directions
+  crypBasisRes = build_crypticDir(Ranges=Ranges,JSR.table=JSR.table)
+  crypBasisDir = crypBasisRes$BasisDir
+  crypJDir.names = colnames(crypBasisDir)
+  out1 = miscGlobalResult$Outlier
+  inputData2 = inputData[,which(! c(1:n) %in% out1)]
+
+  crypPOmat = get_POgivenB(X=inputData2,B=crypBasisDir)
+  crypPO = diag(crypPOmat[apply(abs(crypPOmat),2,which.max),])
+  crypPODir0 = crypBasisDir[,apply(abs(crypPOmat),2,which.max)]
+
+  crypOS0 = abs(crypPO)
+  ks.pval=ks.stat=rep(0,20)
+  for (df in 1:20) {
+    # hist(pchisq(onoff_stat^2,df=df,lower.tail=F),main=df)
+    temp.out=which(crypOS0>sqrt(qchisq(siglev,df=df,lower.tail=F)))
+    temp.zero=which(crypOS0^2 < 1e-10)
+    ks.output=ks.test(crypOS0[which(! c(1:length(crypOS0)) %in% c(temp.out,temp.zero))]^2,pchisq,df)
+    ks.pval[df]=ks.output$p.value
+    ks.stat[df]=ks.output$statistic
+    # cat(paste(df,"|",round(ks.pval[df],digits=5),"|",
+    #           round(ks.stat[df],digits=3)),"\n")
+  }
+  df=which.min(ks.stat)
+  cutoff.here = max(sqrt(qchisq(siglev,df=df,lower.tail=F)),cutoff)
+
+  crypOS = rep(0,n)
+  crypOS[-out1] = crypOS0
+  crypPODir = matrix(0,ncol=n,nrow=nrow(crypPODir0))
+  colnames(crypPODir) = rep(".",n)
+  colnames(crypPODir)[-out1] = colnames(crypPODir0)
+  crypPODir[,-out1] = crypPODir0
+  out2 = which(crypOS>cutoff.here)
+
+  crypResult = data.frame(Outlier=out2,
+                          Statistic=round(crypOS[out2],digits=3),
+                          Region=sapply(colnames(crypPODir)[out2],FUN=function(t){crypBasisRes$BasisDir.pos[which(crypJDir.names==t)]}),
+                          Tag=colnames(crypPODir)[out2])
+
+  ## 2. Detect outliers from window directions
+  out1 = c(miscGlobalResult$Outlier,crypResult$Outlier)
+  exonset = Ranges$lRanges
+  if (nrow(exonset)==1) {
+    exon.base=c(exonset[1,2]:exonset[1,3])
+  } else {
+    exon.base=c()
+    for (i in 1:(nrow(exonset)-1)) {
+      exon.base=c(exon.base,c(exonset[i,2]:exonset[i,3]))
+    }
+  }
+
+  residualData2 = inputData[,which(! c(1:n) %in% out1)]
+  pileupData2 = pileupData[,which(! c(1:n) %in% out1)]
+
+  onoff_res = get_offstat(residualData=residualData2,pileupData=pileupData2,
+                          exonset=exonset,ADcutoff=ADcutoff,
+                          windowSize=windowSize,readconstr=10)
+
+  localOS0 = onoff_res$stat
+  ks.pval=ks.stat=rep(0,20)
+  for (df in 1:20) {
+    # hist(pchisq(onoff_stat^2,df=df,lower.tail=F),main=df)
+    temp.out=which(localOS0>sqrt(qchisq(siglev,df=df,lower.tail=F)))
+    temp.zero=which(localOS0^2 < 1e-10)
+    ks.output=ks.test(localOS0[which(! c(1:length(localOS0)) %in% c(temp.out,temp.zero))]^2,pchisq,df)
+    ks.pval[df]=ks.output$p.value
+    ks.stat[df]=ks.output$statistic
+    # cat(paste(df,"|",round(ks.pval[df],digits=5),"|",
+    #           round(ks.stat[df],digits=3)),"\n")
+  }
+  df=which.min(ks.stat)
+  cutoff.here = max(sqrt(qchisq(siglev,df=df,lower.tail=F)),cutoff)
+
+  localOS = rep(0,n)
+  localOS[-out1] = localOS0
+  localOS[out2] = crypOS[out2]
+  localMOD = matrix(0,ncol=n,nrow=d)
+  localMOD[,-out1] = onoff_res$tMOD
+  localMOD[,out2] = crypPODir[,out2]
+
+  out3 = which(localOS>cutoff.here)
+  out3 = out3[which(! out3 %in% out2)]
+  localOutRegion = apply(matrix(localMOD[,out3],ncol=length(out3)),2,FUN=function(t){collapse_junction(c(min(which(t^2>0)),max(which(t^2>0))))})
+
+  localResult.table = data.frame(Outlier=out3,
+                                 Statistic=round(localOS[out3],digits=3),
+                                 Region=localOutRegion,
+                                 Tag=rep(".",length(out3)))
+  localResult.table = rbind(localResult.table,crypResult)
+  localResult.table = localResult.table[order(localResult.table$Outlier),]
+
+  outputObject = list(table=localResult.table,
+                      Outlier=localResult.table$Outlier,
+                      OS=localOS,
+                      MOD=localMOD)
+  class(outputObject) = append(class(outputObject),"LSCOutput")
+  return(outputObject)
+}
+
+#' Collect directions for potential cryptic events
+#'
+#' @export
+build_crypticDir = function(Ranges,JSR.table) {
+  ## abnormal junction basis (do not consider cryptic events)
+  nexons = dim(Ranges$lRanges)[1]
+  cryptic.idx = which(grepl(pattern="Cryptic",JSR.table[,which(colnames(JSR.table)=="JV.class")])==T)
+  crypJDir.names = rep("0",length(cryptic.idx))
+  crypJDir.pos = rep("0",length(cryptic.idx))
+
+  for (j in 1:length(cryptic.idx)) {
+    i = cryptic.idx[j]
+    JSR.i = JSR.table[i,]
+    LBE.tab = sapply(c(split_junction(as.character(JSR.i$LBE.position))),FUN=function(t){c(strsplit(strsplit(t,":")[[1]][1],"exon")[[1]][2],strsplit(t,":")[[1]][2])})
+    LBE.which = which((! LBE.tab[2,]=="out") & (! LBE.tab[2,]=="0"))
+    if (LBE.which==2) {
+      LBE = as.numeric(LBE.tab[,LBE.which])
+      if (LBE[2]<0) {
+        crypJDir.pos[j] = collapse_junction(c(Ranges$lRanges[LBE[1],2],Ranges$lRanges[LBE[1],2] - LBE[2]-1))
+      } else {
+        crypJDir.pos[j] = collapse_junction(c(Ranges$lRanges[LBE[1],2] - LBE[2],Ranges$lRanges[LBE[1],2]))
+      }
+      crypJDir.names[j] = as.character(JSR.i$Tag)
+    } else {
+      LBE = as.numeric(LBE.tab[,LBE.which])
+      if (LBE[2]<0) {
+        crypJDir.pos[j] = collapse_junction(c(Ranges$lRanges[LBE[1],3] + LBE[2]+1,Ranges$lRanges[LBE[1],3]))
+      } else {
+        crypJDir.pos[j] = collapse_junction(c(Ranges$lRanges[LBE[1],3],Ranges$lRanges[LBE[1],3] + LBE[2]))
+      }
+      crypJDir.names[j] = as.character(JSR.i$Tag)
+    }
+  }
+
+  crypBasisDir = matrix(0,nrow=d,ncol=length(crypJDir.names))
+  colnames(crypBasisDir) = crypJDir.names
+  for (i in 1:length(crypJDir.names)) {
+    jpos = split_junction(crypJDir.pos[i])
+    crypBasisDir[c(jpos[1]:jpos[2]),i] = 1
+  }
+  crypBasisDir = apply(crypBasisDir,2,FUN=function(t){t/sqrt(sum(t^2))})
+  return(list(BasisDir=crypBasisDir,BasisDir.pos=crypJDir.pos))
+}
+
+#' Collect known directions
+#'
+#' @export
+build_knownDir = function(Ranges,JSR.table) {
+  plat.table = build_platTable(Ranges = Ranges,JSR.table = JSR.table)
+  nexons = dim(Ranges$lRanges)[1]
+
+  ## Type 1. sparse plat basis only for exon & intron basis
+  platDir.names = sort(unique(c(sapply(rownames(plat.table),FUN=function(t){strsplit(t,split="[.]")[[1]][1]}),rownames(plat.table)[which(grepl(pattern="I",rownames(plat.table))==T)])))
+  platDir = matrix(0,nrow=dim(plat.table)[1],ncol=length(platDir.names))
+  rownames(platDir) = rownames(plat.table)
+  colnames(platDir) = platDir.names
+  for (i in 1:ncol(platDir)) {
+    ij = which(rownames(plat.table)==platDir.names[i])
+    if (length(ij)>0) {
+      platDir[ij,i] = 1
+    } else {
+      platDir[which(sapply(rownames(plat.table),FUN=function(t){strsplit(t,split="[.]")[[1]][1]})==platDir.names[i]),i] = 1
+    }
+  }
+
+  ## Type 2. abnormal junction basis (do not consider cryptic events)
+  non.cryptic.idx = which(grepl(pattern="Cryptic",JSR.table[,which(colnames(JSR.table)=="JV.class")])==F)
+  JDir = matrix(0,nrow=dim(plat.table)[1],ncol=(2*dim(JSR.table)[1]))
+  rownames(JDir) = rownames(plat.table)
+  JDir.names = c()
+  di = 1
+  for (i in non.cryptic.idx) {
+    JSR.i = JSR.table[i,]
+    LBE.tab = sapply(c(split_junction(as.character(JSR.i$LBE.position))),FUN=function(t){c(strsplit(strsplit(t,":")[[1]][1],"exon")[[1]][2],strsplit(t,":")[[1]][2])})
+    if (sum(grepl(pattern="out",LBE.tab[2,]))>0) {
+      if ((diff(as.numeric(LBE.tab[1,]))==0) & (sum(grepl(pattern="0",LBE.tab[2,]))>0)) {
+        next
+      }
+      exon.bp.tmp = as.numeric(split_junction(as.character(JSR.i$junctions.l)))
+      if (exon.bp.tmp[1]<0) {
+        basei = which((as.numeric(sapply(as.character(plat.table$Range),split_junction)[2,])<exon.bp.tmp[2]) & (plat.table$Domain=="exon"))
+        JDir[basei,di] = 1
+        JDir.names = c(JDir.names,as.character(JSR.i$Tag))
+        di = di + 1
+        if (diff(as.numeric(LBE.tab[1,]))>(nexons/3)) {
+          JDir[which(plat.table$Domain=="exon")[which(! which(plat.table$Domain=="exon") %in% basei)],di] = 1
+          JDir.names = c(JDir.names,as.character(JSR.i$Tag))
+          di = di + 1
+        }
+      } else {
+        basei = which((as.numeric(sapply(as.character(plat.table$Range),split_junction)[1,])>exon.bp.tmp[1]) & (plat.table$Domain=="exon"))
+        JDir[basei,di] = 1
+        JDir.names = c(JDir.names,as.character(JSR.i$Tag))
+        di = di + 1
+        if (diff(as.numeric(LBE.tab[1,]))>(nexons/3)) {
+          JDir[which(plat.table$Domain=="exon")[which(! which(plat.table$Domain=="exon") %in% basei)],di] = 1
+          JDir.names = c(JDir.names,as.character(JSR.i$Tag))
+          di = di + 1
+        }
+      }
+    } else {
+      exon.bp.tmp = as.numeric(split_junction(as.character(JSR.i$junctions.l)))
+      basei = which(((as.numeric(sapply(as.character(plat.table$Range),split_junction)[2,])<exon.bp.tmp[2]) & (as.numeric(sapply(as.character(plat.table$Range),split_junction)[1,])>=exon.bp.tmp[1])) & (plat.table$Domain=="exon"))
+      if (length(basei)>0) {
+        JDir[basei,di] = 1
+        JDir.names = c(JDir.names,as.character(JSR.i$Tag))
+        di = di + 1
+        if (diff(as.numeric(LBE.tab[1,]))>(nexons/3)) {
+          JDir[which(plat.table$Domain=="exon")[which(! which(plat.table$Domain=="exon") %in% basei)],di] = 1
+          JDir.names = c(JDir.names,as.character(JSR.i$Tag))
+          di = di + 1
+        }
+      }
+    }
+    rm(basei)
+  }
+  JDir = JDir[,1:which.max(which(apply(JDir,2,sum)>0))]
+  colnames(JDir) = JDir.names
+
+  ## Type 3. Get SV basis
+  p = dim(plat.table)[1]
+  SVDir = matrix(0,nrow=p,ncol=(2*(nexons-1)))
+  rownames(SVDir) = rownames(plat.table)
+  colnames(SVDir) = rep(paste("SV",1:(nexons-1),sep=""),each=2)
+  for (i in 1:(nexons-1)) {
+    intron.j = which(sapply(rownames(plat.table),FUN=function(t){strsplit(t,"[.]")[[1]][1]})==paste0("I",i))[1]
+    SVDir[which((plat.table$Domain=="exon") & (c(1:p) < intron.j)),(2*(i-1)+1)] = 1
+    SVDir[which((plat.table$Domain=="exon") & (c(1:p) > intron.j)),(2*(i-1)+2)] = 1
+  }
+  ## Collapse
+  knownDir = cbind(platDir,JDir,SVDir)
+  return(knownDir)
+}
+
 #' Collect ATT/ATS directions
 #'
 #' @export
