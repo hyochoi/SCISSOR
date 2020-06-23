ffa81b726f15dc7f95422939c214b86dc6c6d941
lucienne1216@gmail.com <lucienne1216@gmail.com>
2020-06-23 18:10:44 -0500

    read deletion fixed

--- R/read_BAM.R
+++ R/read_BAM.R
@@ -74,12 +74,19 @@ read_aBAM = function(BAM,regions=NULL,strand.specific=FALSE,...) {
   strnd = strsplit(regions,":")[[1]][3]
   df = GRanges(chr,IRanges(start=as.numeric(strtend[,1]), end=as.numeric(strtend[,2])),strnd)
 
+  # https://www.rdocumentation.org/packages/Rsamtools/versions/1.24.0/topics/pileup
   s_param = ScanBamParam(which=df, what=c("pos"))
   # p_param = PileupParam(max_depth=1000, min_base_quality = 0, min_mapq = 0, min_nucleotide_depth = 0, min_minor_allele_depth = 0, distinguish_strands = FALSE, distinguish_nucleotides = FALSE, ignore_query_Ns = FALSE, include_deletions = FALSE, include_insertions = FALSE, left_bins = NULL, query_bins = NULL,  cycle_bins = NULL)
   if (!strand.specific) {
-    res = pileup(bf, scanBamParam=s_param, pileupParam=PileupParam(distinguish_strands=F,distinguish_nucleotides=F,...))
+    res = pileup(bf, scanBamParam=s_param, pileupParam=PileupParam(distinguish_strands=F,distinguish_nucleotides=F,
+                                                                   include_deletions = FALSE, include_insertions = FALSE,
+                                                                   left_bins = NULL, query_bins = NULL,  cycle_bins = NULL,
+                                                                   ...))
   } else {
-    res = pileup(bf, scanBamParam=s_param, pileupParam=PileupParam(distinguish_strands=T,distinguish_nucleotides=F,...))
+    res = pileup(bf, scanBamParam=s_param, pileupParam=PileupParam(distinguish_strands=T,distinguish_nucleotides=F,
+                                                                   include_deletions = FALSE, include_insertions = FALSE,
+                                                                   left_bins = NULL, query_bins = NULL,  cycle_bins = NULL,
+                                                                   ...))
     res = res[which(res$strand=="+"),]
   }
   ## pileup does not report zero coverage. manually put zero coverage to the output.
