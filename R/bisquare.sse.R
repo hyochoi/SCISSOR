faf9ee06500d4d5264ecd37bed642389d7068d0e
Hyo Young Choi <36807038+hyochoi@users.noreply.github.com>
2022-06-07 23:22:28 +0900

    updates

--- R/bisquare.sse.R
+++ R/bisquare.sse.R
@@ -1,19 +1,32 @@
 #'
 bisquare.sse = function(x) {
   sig0 = sd(x)
-  if (sig0<1e-10){
+  if (sig0<1e-10) {
     return(0)
   } else {
     k = 4.685*sig0
-    bisquare.fnt = function(y,k) {
-      c = (k^2)/6
-      if (abs(y)<=k) {
-        return(c*(1-((1-((y/k)^2))^3)))
-      } else {
-        return(c)
-      }
-    }
-    bx = apply(matrix(x,ncol=1),1,FUN=function(x){bisquare.fnt(y=x,k=k)})
+    c = (k^2)/6
+    bx = rep(c, length(x))
+    bx[which(abs(x) <= k)] = c*(1-((1-((bx[which(abs(x) <= k)]/k)^2))^3))
     return(sum(bx))
   }
 }
+# bisquare.sse = function(x) {
+#   sig0 = sd(x)
+#   if (sig0<1e-10){
+#     return(0)
+#   } else {
+#     k = 4.685*sig0
+#     bisquare.fnt = function(y,k) {
+#       c = (k^2)/6
+#       if (abs(y)<=k) {
+#         return(c*(1-((1-((y/k)^2))^3)))
+#       } else {
+#         return(c)
+#       }
+#     }
+#     bx = sapply(x,FUN=function(x){bisquare.fnt(y=x,k=k)})
+#     return(sum(bx))
+#   }
+# }
+
