377789106d34200baa1d4d826952e2ad5313ba3a
Hyo Young Choi <lucienne1216@gmail.com>
2022-07-03 13:37:53 -0500

    error fixed

--- R/pos.sd.R
+++ R/pos.sd.R
@@ -1,14 +1,14 @@
 #'
 #' @export
 pos.sd = function(x,x0=NULL,cutoff=0) {
-  # Find non-zero entries of x0 
+  # Find non-zero entries of x0
   #   and compute variance of x on those entries
   if (is.null(x0)) {
     y = x[x>cutoff]
   } else {
     y = x[x0>cutoff]
   }
-  if (length(y)>0) {
+  if (length(y)>1) {
     return(sd(y))
   } else {
     return(0)
